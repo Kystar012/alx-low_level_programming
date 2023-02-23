02879a9f7598d863bbb667112f57bad3d38824cd
Kystar012 <mwirigifestus4@gmail.com>
2023-02-23 01:38:23 -0800

    0 through 9 exept 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers-prints numbers from 0 to 9 execpt 2 and 4
+ *
+ * Return:0(success)
+ */
+
+void print_most_numbers(void)
+{
+	int b;
+
+for (b = 0; b <= 9; b++)
+{
+	if (b != 2 && b != 4)
+	{
+		_putchar(b);
+	}
+}
+	_putchar('\n');
+}
