c57398ad762efd8209670ead32c64596bbb299c9
Kystar012 <mwirigifestus4@gmail.com>
2023-02-23 04:40:07 -0800

    square on a  terminal.

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square-prints a sqiare followed by a new line
+ * @size:is soze of a square
+ *
+ * Return:0(success)
+ */
+
+void print_square(int size)
+{
+	int l, m;
+
+	for (l = 1; l <= size; l++)
+{
+	for (m = 1; m <= size; m++)
+{
+	_putchar('#');
+}
+for (; size = 0; size < 0)
+{
+	_putchar('\n');
+}
+_putchar('\n');
+}
+}
