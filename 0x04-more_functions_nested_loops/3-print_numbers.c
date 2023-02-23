a282ca68ceb137b733cba9152c8d380180463631
Kystar012 <mwirigifestus4@gmail.com>
2023-02-23 01:28:00 -0800

    numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - print the numbers from 0 to 9 fillowed by new line
+ *
+ * Return:0(success)
+ */
+
+void print_numbers(void)
+{
+	int y;
+
+	for (y = 0; y <= 9; y++)
+	{
+		_putchar(y);
+	}
+	_putchar('\n');
+}
