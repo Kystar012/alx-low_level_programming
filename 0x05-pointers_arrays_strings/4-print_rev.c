4be74b91726fb51eadc6d2e77412a5e7917f36fb
Kystar012 <mwirigifestus4@gmail.com>
2023-02-27 14:31:24 -0800

    task 7

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,7 +11,7 @@ int i = 0;
 
 while (s[i] != '\0')
 	i++;
-	for (i = i - 1; i >= 0; i--)
-		_putchar(s[i]);
-				_putchar('\n');
+for (i = i - 1; i >= 0; i--)
+_putchar(s[i]);
+_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,12 +1,24 @@
 #include "main.h"
 /**
  * puts_half- prints half of  string followed by new line
- * @*str:pointer to string to be printed
+ * @str:pointer to string to be printed
  *
  * Return:void 0
  */
 void puts_half(char *str)
 {
-
+int length = 0;
+while (*str != '\0')
+{
+	length++;
+	str++;
+}
+str -= (length / 2);
+	while (*str != '\0')
+{
+	_putchar(*str);
+	str++;
+}
+_putchar('\n');
 }
 
