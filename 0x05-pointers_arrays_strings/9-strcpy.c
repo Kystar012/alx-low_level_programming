733782a96aa3e062897661addbf869520ab20c6c
Kystar012 <mwirigifestus4@gmail.com>
2023-02-27 14:55:43 -0800

    string copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,21 @@
 #include "main.h"
 /**
- * *_strcpy-copies string of char from source to destination
- * @*dest:where string to be cioied to
- * @*src:source of str to be copied
+ * _strcpy-copies string of char from source to destination
+ * @dest:where string to be cioied to
+ * @src:source of str to be copied
  *
  * Return: 0(success)
  */
 
 char *_strcpy(char *dest, char *src)
 {
+	int length;
 
+	while (*(src + length) != '\0')
+	{
+		*(dest + length) = *(src + length);
+	length++;
+}
+*(dest + length) = '\0';
+return (dest);
 }
