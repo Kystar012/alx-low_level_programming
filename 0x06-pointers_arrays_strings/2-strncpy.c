d0eff5e05dfc73168ffc99f78afbb6a0e55c82ee
Kystar012 <mwirigifestus4@gmail.com>
2023-03-01 11:09:32 -0800

    task 2-copying string src to dest.

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * _strncpy- copies a string
+  *@dest:string to copy to
+  *@src:string to copy from
+  *@n:string length(no.elememts)
+  *
+  *Return:pointer dest.(success)
+  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int l;
+
+	while (l < n && src[l] != '\0')
+	{
+		dest[l] = src[l];
+		l++;
+	}
+	while (l < n)
+	{
+		dest[l] = '\0';
+	l++;
+	}
+return (dest);
+}
+
