72be46db8be77292ecd998a9d717a277f73fc383
Kystar012 <mwirigifestus4@gmail.com>
2023-03-02 10:09:44 -0800

    rot13 cipher

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+  * rot13 -encodes a string using rot13
+  * @n:string to encode
+  *
+  *Return:char(success)
+  */
+char *rot13(char *n)
+{
+int l, m;
+char alpha[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+	'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+	'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char cipher[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+		'i', 'j', 'k', 'l', 'm'};
+
+	while (n[l])
+	{
+	for (m = 0; m < 52; m++)
+	{
+		if (n[l] == alpha[m])
+		{
+			n[l] = cipher[m];
+		}
+	}
+	l++;
+	}
+	return (n);
+}
