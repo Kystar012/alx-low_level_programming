37d97cc2641ebb5f25aae20acf72853044caa9de
Kystar012 <mwirigifestus4@gmail.com>
2023-03-06 05:30:57 -0800

    intended to fill memory with constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * _memset- fills memory with constant byte
+  *@s:pointer of the memory adddress
+  *@b:constant byte to fill the memory
+  *@n:memory area size
+  *
+  *Return :pointer to a memory
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int l;
+char *marea = s, value = b;
+
+for (l = 0; l < n; l++)
+{
+	marea[l] = value;
+}
+	return (marea);
+}
