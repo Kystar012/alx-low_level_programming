f8ec66f15006e22c2fe4be96a2d863f54df08fd1
Kystar012 <mwirigifestus4@gmail.com>
2023-03-08 10:07:13 -0800

    taks0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,12 @@
   */
 void _puts_recursion(char *s)
 {
-	for (; *s != '\0'; )
-	{
+if (*s)
+{
 		_putchar(*s);
 		s++;
-		_puts_recursion(s + 1);
+		_puts_recursion(s);
 }
-			_putchar('\n');
+else
+		_putchar('\n');
 }
