9f7493503a3bcedb0aafbb3ec5bea9af52b80b3a
Kystar012 <mwirigifestus4@gmail.com>
2023-03-08 11:04:32 -0800

    string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,7 +10,9 @@ int _strlen_recursion(char *s)
 	int n = 0; /* @n: is the strong length */
 
 	if (*s)
+	{
 		n = _strlen_recursion(s + 1);
 	return (n = _strlen_recursion(s + 1));
-
+	}
+return (0);
 }
