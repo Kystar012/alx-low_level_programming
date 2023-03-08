ba917bcc1dfa9a27520f9f503fbf87c4b1369e7e
Kystar012 <mwirigifestus4@gmail.com>
2023-03-08 10:26:48 -0800

    rev string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,8 +9,9 @@ void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_putchar(*s % 10 + '0');
-		_print_rev_recursion(s--);
+		_putchar(*s);
+		s--;
+		_print_rev_recursion(s + 1);
 	}
 	else
 		_putchar('\n');
