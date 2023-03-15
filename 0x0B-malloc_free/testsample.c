807d098dcced6636d5358a4fcc155f882503ee34
Kystar012 <mwirigifestus4@gmail.com>
2023-03-15 02:45:27 -0700

    wrote a  function that concatenates two strings.

--- 0x0B-malloc_free/testsample.c
+++ 0x0B-malloc_free/testsample.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * main - check the code for ALX School students.
+ * main - check the code for ALX School students
  *
  * Return: Always 0.
  */
@@ -11,10 +11,10 @@ int main(void)
 {
     char *s;
 
-    s = _strdup("ALX SE");
+    s = str_concat("Betty ", "Holberton");
     if (s == NULL)
     {
-        printf("failed to allocate memory\n");
+        printf("failed\n");
         return (1);
     }
     printf("%s\n", s);
