1a2c99af2267dc107eeeab7e63622e307163fb37
Kystar012 <mwirigifestus4@gmail.com>
2023-03-20 12:57:35 -0700

    creating a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+  *File:dog.h
+  *Auth:Kystar012
+  *Struct dog-ma,e of the function
+  *@name:element of struct dog
+  *@age:element of struct dog
+  *@owner:element of struct dog
+  *Description:The struct dog prints the name,age amd owner
+  */
 struct dog{
 	char *name;
 	float age;
