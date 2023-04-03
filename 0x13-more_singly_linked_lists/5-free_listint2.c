3ede0b0f85207b5b52b3c45f7e9ab8d15610b7bd
root <root@localhost.localdomain>
2023-04-03 16:11:59 +0000

    frees the memory allocated to nodes

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+  *free_listint2-frees memory allocated to jead
+  *@head:pointer to linked list
+  *
+  *Return:void
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		current = *head;
+		free(*head);
+		*head = current->next;
+	}
+	*head = NULL;
+}
