3096179b30e6a31fbade298d173f9c067da07731
root <root@localhost.localdomain>
2023-04-03 15:32:32 +0000

    made few corrections to file to make it give desired results

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,15 +1,16 @@
 #include "lists.h"
+
 /**
  * print_listint-prints all element of listint_t
  * @h:pointer to listint_t
  *
  * Return:number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
-	size_t numnodes;
+	size_t numnodes = 0;
 
-	numnodes = 0;
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
