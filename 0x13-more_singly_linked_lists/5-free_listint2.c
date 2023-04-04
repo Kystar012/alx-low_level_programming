d50ff973c47ca492619336a2e0529a51a1f1cd96
root <root@localhost.localdomain>
2023-04-04 17:00:34 +0000

    made changes to task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,22 +1,21 @@
 #include "lists.h"
 /**
-  *free_listint2-frees memory allocated to jead
+  *free_listint2-frees memory allocated to head
   *@head:pointer to linked list
   *
   *Return:void
   */
 void free_listint2(listint_t **head)
 {
-	listint_t *current;
+	listint_t *current = *head;
 
 	if (head == NULL)
 		return;
 
 	while (*head != NULL)
 	{
-		current = *head;
-		free(*head);
 		*head = current->next;
+		free(*head);
 	}
-	*head = NULL;
+	head = NULL;
 }
