66d1530cab722ca93ef210536842d1981b385762
root <root@localhost.localdomain>
2023-04-04 18:13:46 +0000

    adds a new node anywhere in the list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-		newnode->n = n;	
+		newnode->n = n;
 	if (idx == 0)
 	{
 		newnode->next = copy;
