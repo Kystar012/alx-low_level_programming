319235b11277fd7a3e8d372a685fb43a203a1454
root <root@localhost.localdomain>
2023-04-04 17:05:52 +0000

    made changes to task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,8 +14,9 @@ void free_listint2(listint_t **head)
 
 	while (*head != NULL)
 	{
-		*head = current->next;
-		free(*head);
+		current = *head;
+		*head = (*head)->next;
+		free(current);
 	}
-	head = NULL;
+	*head = NULL;
 }
