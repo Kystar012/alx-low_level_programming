8af88f784e3da0eae9edc68cf14137b942adf84e
root <root@localhost.localdomain>
2023-04-19 20:17:27 +0000

    select the correct function to peeform the operation

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,9 +2,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-  *main-
+  *main-prints the result of operation
+  *@argc:argument count
+  *@argv:argument vector
+  *
+  *Return:results or Error(98,99,100)
   */
-int main(void)
+int main(int argc, char* argv[])
 {
+	int num1, num2;
 
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	char* operator = argv[2];
+	num2 = atoi(argv[3]);
+	if (get_op_func(operator) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+			exit(99);
+	}
+	if ((*operator == '/' && num2 == 0) || (*operator == '%' && num2 == 0))
+	{
+		printf("Error\n");
+			exit(100);
+	}
+	printf("%d\n", get_op_func(operator)(num1, num2));
+	return (0);
 }
