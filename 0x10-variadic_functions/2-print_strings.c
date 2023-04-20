4cd717bf03daee345bdeef147a5b80092ad2f728
root <root@localhost.localdomain>
2023-04-20 19:46:00 +0000

    a function prints a string followed by new line

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(pstr, char *);
 		if (str == NULL)
-			printf("nil");
+			printf("(nil)");
 		else
 			printf("%s", str);
 		if (index != (n - 1) && separator != NULL)
