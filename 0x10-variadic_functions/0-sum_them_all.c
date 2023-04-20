d23b79ae79f9fc30f27eadbeaee1250edd353097
root <root@localhost.localdomain>
2023-04-20 18:39:27 +0000

    function returns sum of all its parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,13 +8,16 @@
   */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0;
 	va_list ap;
 	unsigned int i;
 
-	if (n == 0)
+	while (n == 0)
+	{
 		return (0);
+	}
 	va_start(ap, n);
+	int sum = 0;
+
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(ap, const unsigned int);
