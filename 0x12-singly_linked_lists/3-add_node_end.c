0d8ce227f9887ff656dced3b750cc8d585d7b774
root <root@localhost.localdomain>
2023-04-27 17:05:02 +0000

    adds node at end of list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+*add_node_end-adds node at end of the list_t
+*@head:pointer to first element of list_t
+*@str:pointer to constant character
+*
+*Return:address of last element or null
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	char *copy;
+	int len;
+	list_t *new, *last;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	copy = strdup(str);
+	if (str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (len = 0; str[len];)
+		len++;
+	new->str = copy;
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new;
+	}
+	return (*head);
+}
