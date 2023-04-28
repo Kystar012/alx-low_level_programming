855cd69a0e2faa6a6b6fe2d3567599780a83b2fd
root <root@localhost.localdomain>
2023-04-28 07:41:37 +0000

    modified file to make it execute properly and betty compliant

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-/**
-  *print_string- prints the message intended
-  *
-  *Return:printed string
-  */
 
 void __attribute__((constructor)) print_string(void);
-
+/**
+  *print_string-prints the string
+  *
+  *Return: void
+  */
 void print_string(void)
 {
-printf("You're beat! and yet, you must allow,\n"
-		"
-I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
