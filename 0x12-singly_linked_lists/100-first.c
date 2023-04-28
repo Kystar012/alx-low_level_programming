03238901d9c2b6807e348906fb38fdc2675cd85f
root <root@localhost.localdomain>
2023-04-28 07:35:56 +0000

    modified file to make it betty compliant

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,10 +5,11 @@
   *Return:printed string
   */
 
-void print_string(void) __attribute__((constructor));
+void __attribute__((constructor)) print_string(void);
 
 void print_string(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house
-		upon my back!\n");
+printf("You're beat! and yet, you must allow,\n"
+		"
+I bore my house upon my back!\n");
 }
