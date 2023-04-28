912a446264feab069de9ada0f702c703f88d80e4
Kystar012 <mwirigifestus4@gmail.com>
2023-04-28 02:13:49 -0700

    64-bit program assembler

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -4,26 +4,22 @@
 extern printf
 
 section.data
-    string db "Hello, Holberton", 10, 0
-    frmt db "s", 10, 0
+    string: db "Hello, Holderton", 0Ah, 0
+    format: db "s", 0Ah, 0
 
 section .text
     global main
 
 main:
-;Save base pointer and set up stack frame.
 
     push rbp
-    mov rbp, rsp
-;Call printf with message and format arguments
-
-    mov rdi, frmt
-    mov rsi, string
-    xor rax, rax
+   
+    mov rdi, format
+    mov rdi, string
+    mov rax, 0
     call printf
 
-;Restore base pointer and return
-
     pop rbp
-    xor rax, rax
+
+    mov rax, 0
     ret
